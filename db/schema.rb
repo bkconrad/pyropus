fb096f31a5bd21876617d09a18f5c9c06858b7e8
Bryan Conrad <conrad.bryan.k@gmail.com>
2012-03-11 22:12:48 -0700

    users scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,19 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120312044055) do
+ActiveRecord::Schema.define(:version => 20120312051220) do
 
   create_table "posts", :force => true do |t|
     t.string   "title"
     t.string   "content"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  create_table "users", :force => true do |t|
+    t.string   "username"
+    t.string   "password"
+    t.integer  "group"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
