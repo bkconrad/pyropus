36fe9f8e61c334de90e1ccc4fd220e3349c71541
Bryan Conrad <conrad.bryan.k@gmail.com>
2012-03-11 20:07:52 -0700

    initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pyropus::Application.config.secret_token = '38b30b6cd718b3e115b264a455366f5129f57fc78317a6d44f6ec3c0d4bab6073bdf53e23fb08f45504c95ce72c885dee9a2c0d4d736097d6af8c4e8a87bbb07'
