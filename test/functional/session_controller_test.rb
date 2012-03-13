7bc891add815d5dd38145fdc5a5d94f9b47cebfb
Bryan Conrad <conrad.bryan.k@gmail.com>
2012-03-12 18:39:34 -0700

    added group_id to login tests

--- test/functional/session_controller_test.rb
+++ test/functional/session_controller_test.rb
@@ -11,5 +11,12 @@ class SessionControllerTest < ActionController::TestCase
     alice = users(:alice)
     post :create, name: alice.name, password: "secret"
     assert_equal session[:user_id], alice.id
+    assert_equal session[:group_id], alice.group_id
+  end
+
+  test "should log out" do
+    delete :destroy
+    assert_equal session[:user_id], nil
+    assert_equal session[:group_id], nil
   end
 end
