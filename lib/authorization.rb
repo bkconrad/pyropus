a88845f72bbd77ecb223edb155a625f341a35fbe
Bryan Conrad <conrad.bryan.k@gmail.com>
2012-03-13 13:24:34 -0700

    created a user level checking function and constant array. Not sure this is how I should do this :/

--- lib/authorization.rb
+++ lib/authorization.rb
@@ -1,8 +1,29 @@
 module Authorization
+  USER_LEVELS = [ :unauthenticated,
+                  :normal,
+                  :admin ]
+  # accepts a user, integer or USER_LEVELS value
+  # returns an integer corresponding to a passed user
+  # the index of a passed USER_LEVELS value
+  # or the corresponding USER_LEVELS value of a passed integer
+  def user_level (arg = nil)
+    if @arg == nil
+      return session[:group_id]
+
+    elsif @arg.class == User
+      return @arg.group_id
+
+    elsif USER_LEVELS.index(@arg) != nil
+      return USER_LEVELS.index(@arg)
+
+    end
+    USER_LEVELS[@arg]
+  end
+
   def authorize
     # TODO: figure out how/where to define user level constants
     if session[:group_id] != nil
-      return session[:group_id] > 1
+      return (user_level() > user_level( :admin))
     end
     store_return_to
     redirect_to login_url, alert: "Please login to do that"
