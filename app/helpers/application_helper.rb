d931f64352950bb1e45e7c6488b6e704004cb74b
Bryan Conrad <conrad.bryan.k@gmail.com>
2012-03-14 18:26:46 -0700

    fixed normal usernames not showing up

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,27 +8,13 @@ module ApplicationHelper
   end
 
   def username
-    if user_level == nil
+    if logged_in == nil
       return nil
     end
 
     user = User.find(session[:user_id])
     if user
-      return user.name
-    end
-  end
-
-  def logged_in
-    if session[:user_id] == nil
-      return nil
-    end
-    user = User.find(session[:user_id])
-    return user != nil
-  end
-
-  def admin?
-    if user_level != nil
-      user_level >= user_level(:admin)
+      user.name
     end
   end
 end
