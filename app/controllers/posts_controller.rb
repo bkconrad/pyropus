bc1532b2d5ac43e2395ddd225ea42f804cfd6383
Bryan Conrad <conrad.bryan.k@gmail.com>
2012-03-16 17:28:37 -0700

    Sort posts by descending creation date

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,7 @@ class PostsController < ApplicationController
   # GET /posts
   # GET /posts.json
   def index
-    @posts = Post.all
+    @posts = Post.order("created_at DESC");
     page_title "View all posts"
 
     @posts.each do |post|
