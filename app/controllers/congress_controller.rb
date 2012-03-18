299f88cc23b332196b721c732c824b291779644f
Bryan Conrad <conrad.bryan.k@gmail.com>
2012-03-18 16:15:09 -0700

    spiffied up the bills display a bit

--- app/controllers/congress_controller.rb
+++ app/controllers/congress_controller.rb
@@ -1,15 +1,17 @@
 class CongressController < ApplicationController
-  include NytCongress
+  skip_before_filter :authorize
   caches_page :query
+  include NytCongress
 
   def index
   end
 
   def query
-    if params[:args].length < 1
+    args = params[:args].split("/")
+    if args.length < 1
       # error
       return nil
     end
-    render json: recent_bills("house", "introduced")
+    render json: recent_bills(args[1], "introduced")
   end
 end
