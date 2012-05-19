bce4e6e0d81295c99ae2016cf17eb15ac158b382
Bryan Conrad <conrad.bryan.k@gmail.com>
2012-05-19 14:31:56 -0700

    enable sample updating

--- app/controllers/pfxed_controller.rb
+++ app/controllers/pfxed_controller.rb
@@ -1,5 +1,5 @@
 class PfxedController < ApplicationController
-  skip_before_filter :authorize, only: [:index]
+  skip_before_filter :authorize, only: [:index, :show]
 
   def index
     page_title "Particle Effects"
@@ -14,4 +14,15 @@ class PfxedController < ApplicationController
     end
   end
 
+  def update
+    @sample = PfxedSample.find(params[:id])
+
+    respond_to do |format|
+      if @sample.update_attributes(params[:sample])
+        format.json { render json: @sample }
+      else
+        format.json { render json: @sample.errors, status: :unprocessable_entity }
+      end
+    end
+  end
 end
