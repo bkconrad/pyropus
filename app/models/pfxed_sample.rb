8b28c505c45338feb6c78d7105e949810da55580
Bryan Conrad <conrad.bryan.k@gmail.com>
2012-05-19 18:03:16 -0700

    enable sample renaming

--- app/models/pfxed_sample.rb
+++ app/models/pfxed_sample.rb
@@ -1,3 +1,3 @@
 class PfxedSample < ActiveRecord::Base
-  attr_accessible :init_function, :draw_function, :update_function
+  attr_accessible :init_function, :draw_function, :update_function, :name
 end
