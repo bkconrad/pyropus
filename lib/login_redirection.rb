976e62bf510d61ab077ee1f62f06abc3d8bad3a0
Bryan Conrad <conrad.bryan.k@gmail.com>
2012-05-20 15:31:25 -0700

    add argument passthrough for redirect_back

--- lib/login_redirection.rb
+++ lib/login_redirection.rb
@@ -3,11 +3,11 @@ module LoginRedirection
     session[:return_to] = request.url
   end
 
-  def redirect_back
+  def redirect_back *args
     if session[:return_to]
-      redirect_to session[:return_to]
+      redirect_to session[:return_to], *args
     else
-      redirect_to login_url
+      redirect_to login_url, *args
     end
     session[:return_to] = nil
   end
