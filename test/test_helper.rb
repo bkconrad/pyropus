8260b97b0d55f05632337764119be002aef00345
Bryan Conrad <conrad.bryan.k@gmail.com>
2012-07-20 12:39:58 -0700

    refactor permissions tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,6 +5,48 @@ require 'rails/test_help'
 require 'authorization'
 
 class ActiveSupport::TestCase
+
+  module PermissionsTestHelpers
+    # set the parameters to be passed in access tests
+    def permission_test_params params = nil
+      @permission_test_params = params unless params === nil
+      @permission_test_params
+    end
+
+    # method is a HTTP method
+    # action is a controller method
+    # user can be a fixture or nil to log out
+    def try_as method, action, user
+        log_in user
+        self.send(method, action, permission_test_params)
+    end
+
+    def succeed_as method, action, user
+      try_as method, action, user
+      assert_response 200
+    end
+
+    def fail_as method, action, user
+      try_as method, action, user
+      assert_response 403
+    end
+  end
+
+  module PermissionsDsl
+    def admin_only method, action
+      test "#{action.to_s} is admin only" do
+        fail_as method, action, nil
+        fail_as method, action ,users(:nobody)
+        fail_as method, action ,users(:normal)
+        succeed_as method, action ,users(:admin)
+      end
+    end
+  end
+
+  include PermissionsTestHelpers
+  extend PermissionsDsl
+
+
   # Setup all fixtures in test/fixtures/*.(yml|csv) for all tests in alphabetical order.
   #
   # Note: You'll currently still have to declare fixtures explicitly in integration tests
@@ -13,29 +55,4 @@ class ActiveSupport::TestCase
 
   # Add more helper methods to be used by all tests here...
 
-  # set the parameters to be passed in access tests
-  def permission_test_params params = nil
-    @permission_test_params = params unless params === nil
-    @permission_test_params
-  end
-
-  def self.admin_only method, action
-    test "#{action.to_s} is admin only" do
-      log_out
-      self.send(method, action, permission_test_params)
-      assert_response 403
-
-      log_in users(:nobody)
-      self.send(method, action, permission_test_params)
-      assert_response 403
-
-      log_in users(:normal)
-      self.send(method, action, permission_test_params)
-      assert_response 403
-
-      log_in users(:admin)
-      self.send(method, action, permission_test_params)
-      assert_response 200
-    end
-  end
 end
